5e6f255a277d8d6fc910d6bdf95279b31a9ae3e9
Iman Soltani <iman.soltani.j@gmail.com>
2021-12-08 21:51:11 +0330

    Initialize

---
+++ src/main/kotlin/ir/imancn/modelmapper/ModelMapperApplication.kt
@@ -0,0 +1,11 @@
+package ir.imancn.modelmapper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ModelMapperApplication
+
+fun main(args: Array<String>) {
+	runApplication<ModelMapperApplication>(*args)
+}
